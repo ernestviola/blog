5f9bb77af5d4e2a8d48d497e8755d144937b247e
Ernest V <ernest.viol@gmail.com>
2026-06-05 14:19:24 -0700

    feat: WIP Sign Up page

--- author-frontend/src/routes/Signup/Signup.jsx
+++ author-frontend/src/routes/Signup/Signup.jsx
@@ -1,5 +1,54 @@
+import { useState } from 'react';
+import styles from './Signup.module.css';
+import { Link } from 'react-router-dom';
+
 const Signup = () => {
-  return <></>;
+  const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  console.log(email);
+  return (
+    <div className={styles.signupForm}>
+      <h1>Sign Up</h1>
+      <input
+        type='email'
+        placeholder='email'
+        name='email'
+        aria-label='Email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type='text'
+        placeholder='username'
+        name='username'
+        aria-label='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='password'
+        name='password'
+        aria-label='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='confirm password'
+        name='confirm-password'
+        aria-label='Confirm Password'
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <span>
+        Already have an account? <Link to={'/login'}>Log In.</Link>
+      </span>
+    </div>
+  );
 };
 
 export default Signup;
--- author-frontend/src/routes/routes.jsx
+++ author-frontend/src/routes/routes.jsx
@@ -1,5 +1,11 @@
 import App from '../App.jsx';
+import Login from '@routes/Login';
+import Signup from '@routes/Signup';
 
-const routes = [{ path: '/', element: <App /> }];
+const routes = [
+  { path: '/', element: <App /> },
+  { path: '/signup', element: <Signup /> },
+  { path: '/login', element: <Login /> },
+];
 
 export default routes;
