27772a8cc97ce2f97a6a8d2fe08a988df3a097bf
Ernest V <ernest.viol@gmail.com>
2026-06-17 19:33:13 -0700

    feat: Fetch blog from ID and style
    Copied author-reader/edit and updated to be read only. Removed the auth
    fetch for blogs so authors can only see published blogs.

---
+++ reader-frontend/src/components/Editor/Editor.jsx
@@ -0,0 +1,36 @@
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { Markdown } from '@tiptap/markdown';
+import { Placeholder } from '@tiptap/extensions';
+import styles from './editor.module.css';
+import './tiptapGlobals.css';
+
+const Editor = ({ editable = true, onChange, initialMarkdown = '' }) => {
+  const editor = useEditor({
+    content: initialMarkdown,
+    editable,
+    contentType: 'markdown',
+    extensions: [
+      StarterKit,
+      Markdown.configure({
+        indentation: {
+          style: 'space', // 'space' or 'tab'
+          size: 2, // Number of spaces or tabs
+        },
+      }),
+      Placeholder.configure({
+        placeholder: 'Write some markdown...',
+      }),
+    ],
+
+    onUpdate: ({ editor }) => {
+      onChange?.(editor.getMarkdown());
+    },
+  });
+
+  if (!editor) return null;
+
+  return <EditorContent className={styles.editor} editor={editor} />;
+};
+
+export default Editor;
--- reader-frontend/src/routes/Blogs/View/index.jsx
+++ reader-frontend/src/routes/Blogs/View/index.jsx
@@ -1,7 +1,93 @@
+import Editor from '@components/Editor';
+import { useState, useEffect, useCallback } from 'react';
 import styles from './view.module.css';
+import { authFetch } from '@utils/auth.js';
+import { useNavigate, useParams } from 'react-router';
+import Loading from '@components/Loading';
+import Toast from '@components/Toast';
+import BackButton from '@components/BackButton';
 
 const View = () => {
-  return <div className={styles.container}>View</div>;
+  const navigate = useNavigate();
+
+  const { blogId } = useParams();
+  const [blog, setBlog] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [toasts, setToasts] = useState([]);
+
+  useEffect(() => {
+    document.title = blog?.title ?? '';
+  }, [blog]);
+
+  useEffect(() => {
+    async function fetchBlogData() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/blogs/${blogId}`,
+          {
+            method: 'GET',
+          },
+        );
+
+        if (!response.ok) {
+          throw new Error('Issues retrieving the blog');
+        }
+
+        const data = await response.json();
+        setBlog(data.blog);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchBlogData();
+  }, [blogId, navigate]);
+
+  const addToast = useCallback((status) => {
+    const id = crypto.randomUUID();
+    setToasts((prev) => [...prev, { id, status }]);
+  }, []);
+
+  const removeToast = useCallback((id) => {
+    setToasts((prev) => prev.filter((t) => t.id !== id));
+  }, []);
+
+  if (blog === null) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.toasts}>
+          {toasts.map((toast) => {
+            return (
+              <Toast
+                key={toast.id}
+                color='#55ff'
+                id={toast.id}
+                removeToast={removeToast}
+                msUntilRemoval={1000}
+              >
+                {toast.status}
+              </Toast>
+            );
+          })}
+        </div>
+
+        <div className={styles.header}>
+          <BackButton />
+        </div>
+        <div className={styles.body}>
+          <h1>{blog.title}</h1>
+          <Editor editable={false} initialMarkdown={blog.body} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default View;
