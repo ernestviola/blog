391ed5541ee1f81ecaedabd63105fa35756e754c
Ernest V <ernest.viol@gmail.com>
2026-06-19 19:31:38 -0700

    fix: Comment like count
    Had to replace the old like count with the new one.

--- reader-frontend/src/components/Comments/CommentItem/index.jsx
+++ reader-frontend/src/components/Comments/CommentItem/index.jsx
@@ -1,14 +1,25 @@
+import { BiHeart } from 'react-icons/bi';
+import { BsEye } from 'react-icons/bs';
+
 import Editor from '@components/Editor';
 import styles from './commentItem.module.css';
 
 const CommentItem = ({ data }) => {
+  console.log(data);
   return (
     <div className={styles.container}>
       <div className={styles.username}>@{data.username.username}: </div>
       <div className={styles.commentContainer}>
         <Editor color='white' editable={false} initialMarkdown={data.body} />
       </div>
-      <div className={styles.footer}></div>
+      <div className={styles.footer}>
+        <div className={styles.heartContainer}>
+          <button className={styles.heartButton}>
+            <BiHeart />
+          </button>
+          <span>{data._count.commentLikes}</span>
+        </div>
+      </div>
     </div>
   );
 };
